c5cf2a150d85550b06aa31f402412b9f93a60dbd
Ayelen Melo <ayelen_melo@hotmail.com>
2023-07-16 23:05:43 -0300

    Cambios

---
+++ js/comienza-juego.js
@@ -0,0 +1,34 @@
+// Niveles dinámicamente
+escribeNiveles();
+
+// Asignacion de eventos iniciales
+document.querySelectorAll(".reiniciar").forEach(function(elemento) {
+  elemento.addEventListener("click", reiniciar);
+});
+
+document
+  .querySelector("#juego-normal")
+  .addEventListener("click", iniciaJuegoNormal);
+document
+  .querySelector("#juego-relax")
+  .addEventListener("click", iniciaJuegoRelax);
+
+document
+  .querySelector("#control-nivel")
+  .addEventListener("click", muestraMenuNiveles);
+document
+  .querySelector("#cierra-niveles")
+  .addEventListener("click", ocultaMenuNiveles);
+
+document.querySelectorAll(".nivel").forEach(function(elemento) {
+  elemento.addEventListener("click", cambiaNivel);
+});
+
+document.querySelector("#subir").addEventListener("click", cargaNuevoNivel);
+
+document.querySelector("body").addEventListener("click", clickFueraDeMenu);
+
+document.addEventListener("keydown", teclaEscCierraMenu);
+
+//Pantalla de bienvenida
+document.querySelector("#bienvenida").classList.add("visible");
